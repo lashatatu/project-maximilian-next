9d278afc11a48728fe7003a5227cef2f127e9f39
lasha tatulashvili <lashatatu@gmail.com>
2021-03-25 03:33:55 +0400

    77 all events

---
+++ components/layout/main-header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Link from 'next/link';
+import classes from './main-header.module.css'
+
+function MainHeader (props) {
+	return (
+		 <header className={classes.header}>
+			 <div className={classes.logo}>
+				 <Link href={'/'}>
+					 NextEvents
+				 </Link>
+			 </div>
+			 <nav className={classes.navigation}>
+				 <ul>
+					 <li>
+						 <Link href={'/events'}>browse all events</Link>
+					 </li>
+				 </ul>
+			 </nav>
+		 </header>
+	);
+}
+
+export default MainHeader;
