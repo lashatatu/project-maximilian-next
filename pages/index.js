3273767117c6e768ae009b89b7eabc4e3b170fd9
lasha tatulashvili <lashatatu@gmail.com>
2021-03-24 23:35:14 +0400

    73 styling

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import EventList from '../components/events/event-list';
+import {getFeaturedEvents} from '../dummy-data';
 
 function MainPage (props) {
+
+	const featuredEvents=getFeaturedEvents()
+
 	return (
 		 <div>
-			 <h1>Main page next </h1>
+			 <EventList items={featuredEvents}/>
 		 </div>
 	);
 }
