7bbcf7e0ae49b24f119182799efa7acc36031f31
lasha tatulashvili <lashatatu@gmail.com>
2021-03-25 04:06:33 +0400

    80 extracting data

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,17 +1,25 @@
 import React, {Fragment} from 'react';
+import {useRouter} from 'next/router';
 
 import {getAllEvents} from '../../dummy-data';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 
 function AllEventsPage (props) {
-
+	const router = useRouter();
 	const events = getAllEvents();
+
+	function findEventsHandler (year, month) {
+
+		const fullPath=`/events/${year}/${month}`
+
+		router.push(fullPath);
+	}
 
 	return (
 		 <Fragment>
-			 <EventsSearch/>
-			 <EventList items={events}/>
+			 <EventsSearch onSearch={findEventsHandler} />
+			 <EventList items={events} />
 		 </Fragment>
 	);
 }
