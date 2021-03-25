34f861dcd4b9dba3003d243104e13a17ab19009f
lasha tatulashvili <lashatatu@gmail.com>
2021-03-25 06:08:01 +0400

    81 final steps

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,5 +1,10 @@
 import {useRouter} from 'next/router';
 import {getFilteredEvents} from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+import {Fragment} from 'react';
+import ResultsTitle from '../../components/events/results-title';
+import Button from '../../components/ui/button';
+import ErrorAlert from '../../components/ui/error-alert';
 
 function FilteredEventsPage (props) {
 
@@ -17,24 +22,46 @@ function FilteredEventsPage (props) {
 	const numYear = +filteredYear;
 	const numMonth = +filteredMonth;
 
-	if ( isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12 ){
-		return <p>invalid filter</p>
-	}
-
-	const filteredEvents=getFilteredEvents({
-		year:numYear,
-		month:numMonth
-	})
-
-	if(!filteredEvents|| filteredEvents.length===0){
-		return <p>no events found</p>
-	}
-
+	if ( isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12 ) {
 		return (
-			 <div>
-				 <h1>Filtered events</h1>
-			 </div>
-		);
+			 <Fragment>
+				 <ErrorAlert>
+					 <p>Invalid filter. please adjust your values</p>
+				 </ErrorAlert>
+				 <div className={'center'}>
+					 <Button link={'/events'}>show all events</Button>
+				 </div>
+			 </Fragment>
+		)
+	}
+
+	const filteredEvents = getFilteredEvents({
+		year: numYear,
+		month: numMonth
+	});
+
+	if ( !filteredEvents || filteredEvents.length === 0 ) {
+		return(
+			 <Fragment>
+				 <ErrorAlert>
+					 <p>no events found for the chosen filter</p>
+				 </ErrorAlert>
+
+				 <div className={'center'}>
+					 <Button link={'/events'}>show all events</Button>
+				 </div>
+			 </Fragment>
+		)
+	}
+
+	const date = new Date(numYear, numMonth - 1);
+
+	return (
+		 <Fragment>
+			 <ResultsTitle date={date}/>
+			 <EventList items={filteredEvents} />
+		 </Fragment>
+	);
 }
 
 export default FilteredEventsPage;
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -4,6 +4,7 @@ import {Fragment} from 'react';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
+import ErrorAlert from '../../components/ui/error-alert';
 
 function EventDetailPage (props) {
 
@@ -13,7 +14,11 @@ function EventDetailPage (props) {
 	const event = getEventById(eventId);
 
 	if ( !event ) {
-		return <p>no event found</p>;
+		return (
+			 <ErrorAlert>
+				 <p>no event found</p>
+			 </ErrorAlert>
+		);
 	}
 
 	return (
