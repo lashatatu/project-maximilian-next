686ea194a451dde70607083656ea4594cf3fc89d
lasha tatulashvili <lashatatu@gmail.com>
2021-04-05 08:43:00 +0400

    126

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
+import Head from 'next/head';
+import Link from 'next/link';
 import EventList from '../components/events/event-list';
 import {getFeaturedEvents} from '../helpers/api-utils';
 
 function MainPage (props) {
-
 	return (
 		 <div>
+			 <Head>
+				 <title>Events with Lasha</title>
+				 <meta name={'description'} content={'find great events'}/>
+			 </Head>
 			 <EventList items={props.events} />
 		 </div>
 	);
