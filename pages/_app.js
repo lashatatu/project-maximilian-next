399c434161e9bcb91b4e5a3523638e3aadee638d
lasha tatulashvili <lashatatu@gmail.com>
2021-04-06 17:48:59 +0400

    168

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import Head from 'next/head';
 
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
-import Notification from '../components/ui/notification';
 import { NotificationContextProvider } from '../store/notification-context';
 
 function MyApp ({
@@ -22,10 +21,7 @@ function MyApp ({
 							content="initial-scale=1.0, width=device-width" />
 				 </Head>
 				 <Component {...pageProps} />
-				 <Notification
-						title={'test'}
-						message={'this is a test'}
-						status={'pending'} />
+
 			 </Layout>
 		 </NotificationContextProvider>
 	);
