157afe3c892dd2cac7f40d877f9cc12f1e27f1fd
lasha tatulashvili <lashatatu@gmail.com>
2021-04-06 14:32:21 +0400

    152

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,18 @@
-import {MongoClient} from 'mongodb';
+import { MongoClient } from 'mongodb';
+
+async function connectDatabase () {
+	const client = await MongoClient.connect(
+		 'mongodb+srv://lashatatu:zr1wZbHY6XEc38JY@lashamax.b0l9y.mongodb.net/events?retryWrites=true&w=majority'
+	);
+
+	return client;
+}
+
+async function insertDocument (client, document) {
+	const db = client.db();
+
+	await db.collection('newsletter').insertOne({document});
+}
 
 async function handler (req, res) {
 	if ( req.method === 'POST' ) {
@@ -9,14 +23,22 @@ async function handler (req, res) {
 			return;
 		}
 
-		const client = await MongoClient.connect(
-			 'mongodb+srv://lashatatu:zr1wZbHY6XEc38JY@lashamax.b0l9y.mongodb.net/events?retryWrites=true&w=majority'
-		);
-		const db = client.db();
+		let client;
 
-		await db.collection('newsletter').insertOne({email: userEmail});
+		try {
+			const client = await connectDatabase();
+		} catch ( error ) {
+			res.status(500).json({message:'failed to connect database'})
+			return
+		}
 
-		client.close();
+		try {
+			await insertDocument(client, {email: userEmail});
+			client.close();
+		} catch ( error ) {
+			res.status(500).json({message:'inserting data failed'})
+			return
+		}
 
 		res.status(201).json({message: 'email sent'});
 	}
