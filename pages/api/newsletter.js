ef6f0b82ccaa11b16bd06b7ff8228b3999deeeb2
lasha tatulashvili <lashatatu@gmail.com>
2021-04-06 13:44:55 +0400

    152

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler (req, res) {
+import {MongoClient} from 'mongodb';
+
+async function handler (req, res) {
 	if ( req.method === 'POST' ) {
 		const userEmail = req.body.email;
 
@@ -7,7 +9,14 @@ function handler (req, res) {
 			return;
 		}
 
-		console.log(userEmail);
+		const client = await MongoClient.connect(
+			 'mongodb+srv://lashatatu:zr1wZbHY6XEc38JY@lashamax.b0l9y.mongodb.net/newsletter?retryWrites=true&w=majority'
+		);
+		const db = client.db();
+
+		await db.collection('emails').insertOne({email: userEmail});
+
+		client.close();
 
 		res.status(201).json({message: 'email sent'});
 	}
